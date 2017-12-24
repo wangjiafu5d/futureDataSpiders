8dbf4df09318a27f3e52bdb515594ece6e2e3c1e
chuan <271127035@qq.com>
2017-12-24 21:55:00 +0800

    优化代码

--- src/futureDataSpiders/Spiders.java
+++ src/futureDataSpiders/Spiders.java
@@ -3,6 +3,7 @@ package futureDataSpiders;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Calendar;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -13,38 +14,47 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 public class Spiders {
 	public static void main(String[] args) throws IOException {
 		// String path = "C:\\Users\\chuan\\Downloads\\phantomjs\\bin\\";
+		long start = System.currentTimeMillis();
 		System.setProperty("phantomjs.binary.path", "C:/Users/chuan/Downloads/phantomjs/bin/");
 		File file = new File("C:\\Users\\chuan\\Downloads\\phantomjs\\bin\\phantomjs.exe");
 		System.setProperty("phantomjs.binary.path", file.getAbsolutePath());
 		WebDriver driver = new PhantomJSDriver();
-		for (int j = 0; j < 30; j++) {
-			String date = "2017-9-" + (30 - j);
-			String url = "http://www.100ppi.com/sf/day-" + date + ".html";
-			// String exec = path+"phantomjs.exe "+path+"code.js " + url;
-			driver.get(url);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			Document doc = Jsoup.parse(driver.getPageSource());
-			Elements trs = doc.getElementById("fdata").select("tr");
-			for (int i = 0; i < trs.size(); i++) {
-				String s = trs.get(i).text();
-				char[] test = s.toCharArray();
-				if (isChinese(test[0]) && test.length > 25) {
-					String fileName = s.split(" ")[0];
-					String file2 = "C:\\Users\\chuan\\desktop\\futureData\\" + fileName + ".txt";
-					FileOutputStream fos = new FileOutputStream(file2, true);
-					fos.write((s + " " + date).getBytes());
-					fos.write("\r\n".getBytes());
-					fos.flush();
-					fos.close();
+
+		for (int m = 12; m > 0; m--) {
+			int monthDays = getMaxDayByYearMonth(2017, m);
+			for (int j = 0; j < monthDays; j++) {
+				String date = "2017-" + m + "-" + (monthDays - j);
+				String url = "http://www.100ppi.com/sf/day-" + date + ".html";
+				// String exec = path+"phantomjs.exe "+path+"code.js " + url;
+				driver.get(url);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				Document doc = Jsoup.parse(driver.getPageSource());
+				Elements trs = doc.getElementById("fdata").select("tr");
+				for (int i = 0; i < trs.size(); i++) {
+					String s = trs.get(i).text();
+					if (isChinese(s.charAt(0)) && s.length() > 25) {
+						String fileName = s.split(" ")[0];
+						String filePath = "C:\\Users\\chuan\\desktop\\futureData\\" + fileName + ".txt";
+						File dataFile = new File(filePath);
+						if (!dataFile.exists()) {
+							dataFile.createNewFile();
+						}
+						FileOutputStream fos = new FileOutputStream(filePath, true);
+						fos.write((s + " " + date).getBytes());
+						fos.write("\r\n".getBytes());
+						fos.flush();
+						fos.close();
+					}
 				}
 			}
 		}
 		driver.quit();
-
+		long end = System.currentTimeMillis();
+		System.out.println("程序总共用时：" + (end - start) / 1000.0 + " 秒");
 	}
 
 	private static final boolean isChinese(char c) {
@@ -58,5 +68,12 @@ public class Spiders {
 			return true;
 		}
 		return false;
+	}
+
+	public static int getMaxDayByYearMonth(int year, int month) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month - 1);
+		return calendar.getActualMaximum(Calendar.DATE);
 	}
 }
